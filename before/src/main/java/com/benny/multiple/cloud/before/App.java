f976888232b9563830436fe28ccfee373307e645
benny <178542285@qq.com>
2023-08-02 00:25:10 +0800

    改造后项目,添加配置文件

--- before/src/main/java/com/benny/multiple/cloud/before/App.java
+++ before/src/main/java/com/benny/multiple/cloud/before/App.java
@@ -1,7 +1,6 @@
 package com.benny.multiple.cloud.before;
 
 import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,7 +10,6 @@ import org.springframework.context.ConfigurableApplicationContext;
  */
 @SpringBootApplication
 @EnableNacosConfig
-@NacosPropertySource(dataId = "example", autoRefreshed = true)  // 加载dataId
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
---
+++ springcloud-demo/src/main/java/com/benny/multiple/cloud/StringCloudDemo.java
@@ -0,0 +1,15 @@
+package com.benny.multiple.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+
+@SpringBootApplication
+public class StringCloudDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(StringCloudDemo.class, args);
+    }
+}
