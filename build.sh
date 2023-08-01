ca6a301bbfd9cc438cb4dba6a2728131bb973fd7
benny <178542285@qq.com>
2023-08-01 16:30:41 +0800

    改造后项目,添加配置文件

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 git pull
-docker build --build-a=$1 -t multiple-cloud:v1 .
+docker build --build-arg PROJECT_NAME=$1 -t multiple-cloud:v1 .
 
 docker tag multiple-cloud:v1  registry.cn-hongkong.aliyuncs.com/hikbs/multiple-cloud:v1
 docker push  registry.cn-hongkong.aliyuncs.com/hikbs/multiple-cloud:v1
