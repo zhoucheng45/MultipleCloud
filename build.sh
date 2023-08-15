a645364334bc4104ced5ede28cb8740072e54d97
benny <178542285@qq.com>
2023-08-15 23:34:35 +0800

    启用阿里云镜像仓库

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ git pull
 img_version=v1$1
 docker build -f Dockerfile$1 -t multiple-cloud:$img_version .
 
-docker tag multiple-cloud:v1  registry.cn-hongkong.aliyuncs.com/hikbs/multiple-cloud:$img_version
-docker push  registry.cn-hongkong.aliyuncs.com/hikbs/multiple-cloud:$img_version
+docker tag multiple-cloud:v1  registry-intl.cn-hongkong.aliyuncs.com/my-link/multiple-cloud:$img_version
+docker push  registry-intl.cn-hongkong.aliyuncs.com/my-link/multiple-cloud:$img_version
