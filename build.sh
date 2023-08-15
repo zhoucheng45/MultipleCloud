f745fb5cf842cd53f8ea759a60f7c963006d8be7
benny <178542285@qq.com>
2023-08-15 23:23:42 +0800

    打包

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 git pull
-docker build -t multiple-cloud:v1 .
+img_version=v1$1
+docker build -f Dockerfile$1 -t multiple-cloud:$img_version .
 
-docker tag multiple-cloud:v1  registry.cn-hongkong.aliyuncs.com/hikbs/multiple-cloud:v1
-docker push  registry.cn-hongkong.aliyuncs.com/hikbs/multiple-cloud:v1
+docker tag multiple-cloud:v1  registry.cn-hongkong.aliyuncs.com/hikbs/multiple-cloud:$img_version
+docker push  registry.cn-hongkong.aliyuncs.com/hikbs/multiple-cloud:$img_version
