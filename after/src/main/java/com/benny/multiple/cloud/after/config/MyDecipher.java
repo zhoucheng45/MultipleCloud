670bd0b3ff6c90903a78dc7d6873cfbb36fc0114
benny <178542285@qq.com>
2023-09-05 15:31:39 +0800

    after 集成华为MAS的redis

--- after/src/main/java/com/benny/multiple/cloud/after/config/MyDecipher.java
+++ after/src/main/java/com/benny/multiple/cloud/after/config/MyDecipher.java
@@ -2,11 +2,15 @@ package com.benny.multiple.cloud.after.config;
 
 import com.benny.multiple.cloud.after.AES256Encrypt;
 import com.huawei.devspore.mas.password.Decipher;
+import org.apache.commons.lang3.StringUtils;
 
 public class MyDecipher implements Decipher {
 
     @Override
     public String decode(String s) {
+        if(StringUtils.isBlank(s)){
+            return "";
+        }
         String decrypt = AES256Encrypt.decrypt(s);
         return decrypt;
     }
