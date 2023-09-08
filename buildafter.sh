6856ba15cc9344da1b46ccd8ee185b934b578ba2
Benny <178542285@qq.com>
2023-09-08 19:36:08 +0800

    after 集成华为MAS的redis

--- buildafter.sh
+++ buildafter.sh
@@ -2,7 +2,7 @@ git pull
 img_version=v1$1
 
 cd $1
-RUN mvn clean package -DskipTests=true
+mvn clean package -DskipTests=true
 docker build -f Dockerfileafter1 -t multiple-cloud:$img_version .
 
 docker tag multiple-cloud:$img_version  registry-intl.cn-hongkong.aliyuncs.com/my-link/multiple-cloud:$img_version
